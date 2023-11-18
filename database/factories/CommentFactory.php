ef5bac3bb9285300cd7adb56a832b73f53850a24
Serhii Chechel <chechel@mail.ru>
2023-11-18 10:28:19 +0200

    first commit

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'subject' => $this->faker->sentence(3, true),
+            'body' => $this->faker->paragraph(3, false),
+        ];
+    }
+}
